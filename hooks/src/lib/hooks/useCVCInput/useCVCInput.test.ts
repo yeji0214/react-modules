2074acb5e255e953d0bf555d6ba0e4ecb22dc421
yeji0214 <1225yj50@naver.com>
2025-05-01 13:48:33 +0900

    test: useCVCInput 훅 테스트
    Co-authored-by: mlnwns <mlnwns@users.noreply.github.com>

---
+++ hooks/src/lib/hooks/useCVCInput/useCVCInput.test.ts
@@ -0,0 +1,54 @@
+import { renderHook, act } from "@testing-library/react";
+import useCVCInput from "./useCVCInput";
+import ERROR_MESSAGE from "../constants/errorMessage";
+import { CARD_INPUT } from "../constants/cardValidationInfo";
+
+describe("useCVCInput", () => {
+  it("초기 상태는 빈 문자열 배열이며 유효하지 않음", () => {
+    const { result } = renderHook(() => useCVCInput());
+
+    expect(result.current.cvcState).toEqual({ value: "", isValid: true });
+    expect(result.current.errorMessage).toBe("");
+  });
+
+  it("모든 인풋이 숫자 3자리일 때 유효성 통과", async () => {
+    const { result } = renderHook(() => useCVCInput());
+
+    act(() => {
+      result.current.handleInputChange({
+        target: { value: "123" },
+      } as React.ChangeEvent<HTMLInputElement>);
+    });
+
+    expect(result.current.cvcState).toEqual({ value: "123", isValid: true });
+    expect(result.current.errorMessage).toBe("");
+  });
+});
+
+it("숫자가 아닌 값이 포함되면 유효하지 않음", () => {
+  const { result } = renderHook(() => useCVCInput());
+
+  act(() => {
+    result.current.handleInputChange({
+      target: { value: "ab" },
+    } as React.ChangeEvent<HTMLInputElement>);
+  });
+
+  expect(result.current.cvcState.isValid).toBe(false);
+  expect(result.current.errorMessage).toBe(ERROR_MESSAGE.REQUIRE.NUMBER);
+});
+
+it("길이가 부족한 값이 포함되면 유효하지 않음", () => {
+  const { result } = renderHook(() => useCVCInput());
+
+  act(() => {
+    result.current.handleInputChange({
+      target: { value: "1" },
+    } as React.ChangeEvent<HTMLInputElement>);
+  });
+
+  expect(result.current.cvcState.isValid).toBe(false);
+  expect(result.current.errorMessage).toBe(
+    `숫자 ${CARD_INPUT.MAX_LENGTH.CVC}${ERROR_MESSAGE.REQUIRE.SPECIFIC_LENGTH}`
+  );
+});
